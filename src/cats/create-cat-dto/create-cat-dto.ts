6aeb0b143371007f5ed555780dfb779b0dbbf900
Dmitriy Valkov <dlcwalkoff@gmail.com>
2018-06-15 22:54:54 +0300

    enhance: create Cat interface, update Cat DTO

--- src/cats/create-cat-dto/create-cat-dto.ts
+++ src/cats/create-cat-dto/create-cat-dto.ts
@@ -1,9 +1,11 @@
-export class CreateCatDto {
+import { ICat } from '../interfaces/cat.interface';
+
+export class CatDTO implements ICat {
   readonly name: string;
   readonly age: number;
   readonly breed: string;
 
-  constructor(data: CreateCatDto) {
+  constructor(data: CatDTO) {
     this.name = data.name;
     this.age = data.age;
     this.breed = data.breed;
