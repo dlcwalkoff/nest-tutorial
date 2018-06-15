d755204cc70e9b67f3e7aee925a5b9816bde4f26
Dmitriy Valkov <dlcwalkoff@gmail.com>
2018-06-15 21:03:13 +0300

    enhance: add Cats ctrl

---
+++ src/cats/cats.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CatsController } from './cats.controller';
+
+describe('Cats Controller', () => {
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [CatsController],
+    }).compile();
+  });
+
+  it('should be defined', () => {
+    const controller: CatsController = module.get<CatsController>(CatsController);
+    expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return "This action returns all cats"', () => {
+      const appController = module.get<CatsController>(CatsController);
+      expect(appController.findAll()).toBe('This action returns all cats');
+    });
+  });
+});
